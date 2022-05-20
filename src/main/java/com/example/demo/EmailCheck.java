97a3de09a2c8fccd3b7fc4d76ced642a42cef11c
seohyeon1578 <seohyeon1578@naver.com>
2022-05-20 21:43:39 +0900

    회원가입 이메일 아이디 중복 체크

---
+++ src/main/java/com/example/demo/EmailCheck.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/EmailCheck")
+public class EmailCheck extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doHandle(request,response);
+    }
+
+    protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        String userEmail = request.getParameter("userEmail");
+        System.out.println(userEmail);
+        PrintWriter pOut = response.getWriter();
+
+        MemberDB md = new MemberDB();
+
+        int emailCheck = md.docheckEmail(userEmail);
+
+        if (emailCheck == 0) {
+            System.out.println("이미 존재하는 이메일");
+        } else if (emailCheck == 1) {
+            System.out.println("사용 가능한 이메일");
+        }
+
+        pOut.write(emailCheck + "");
+    }
+}
---
+++ src/main/java/com/example/demo/IdCheck.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/IdCheck")
+public class IdCheck extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doHandle(request,response);
+    }
+
+    protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        String username = request.getParameter("userId");
+        System.out.println(username);
+        PrintWriter pOut = response.getWriter();
+
+        MemberDB md = new MemberDB();
+
+        int idCheck = md.docheckId(username);
+
+        if (idCheck == 0) {
+            System.out.println("이미 존재하는 아이디");
+        } else if (idCheck == 1) {
+            System.out.println("사용 가능한 아이디");
+        }
+
+        pOut.write(idCheck + "");
+    }
+}
